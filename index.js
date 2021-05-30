cd73f9469c4eb1ffe1a411afbc728ccfe9fa2b58
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-30 17:19:57 +1000

    added the function to listen to events, added for loop to cycle through all names and saved that as a constant. Set up to console log currentName to lower case in console.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent.toLocaleLowerCase();
+        console.log(currentName)
+    }
 });
